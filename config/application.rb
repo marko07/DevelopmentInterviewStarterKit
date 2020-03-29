9d351f7606673e9185e5f4afea14ac65cc808752
Marco Pacheco <marcopache@gmail.com>
2020-03-29 15:15:32 -0600

    Add autoloading for files under lib/
    We are creating some files under the lib folder for the calls to
    SalesLoft Public API.
    
    We need to be sure that rails once it starts all the files under
    lib are loaded so they are available.

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,9 @@ module SalesLoftIntegration
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths = %W(
+      #{Rails.root}/lib/
+    ) + config.autoload_paths
   end
 end
